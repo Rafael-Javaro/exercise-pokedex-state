1568b71021d5782ffc3025a065f132b003377ffd
Rafael <rafaeljavaro750@gmail.com>
2021-02-17 22:47:59 -0300

    Cria Botao Fire

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pokemon from './Pokemon';
 
-class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
-    }
-}
+export default class Pokedex extends Component {
+  constructor() {
+    super();
 
-export default Pokedex;
+    this.state = {
+      pokemonType: '',
+    }
+
+    this.handleFilterByFire = this.handleFilterByFire.bind(this);
+  }
+
+  handleFilterByFire() {
+    console.log('Fogo!') 
+  }
+
+  render() {
+    return (
+    <>
+      <div className="pokedex">
+        {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+      </div>
+      <button className="button" onClick={this.handleFilterByFire}>Fire</button>
+    </>
+    );
+  }
+}
